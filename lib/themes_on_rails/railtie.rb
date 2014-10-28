15d249783fe571e8d699eda536fd65eb9d46bbdf
Dan Cheung <dcheung@users.noreply.github.com>
2014-10-27 17:45:00 -0700

    Update railtie.rb

--- lib/themes_on_rails/railtie.rb
+++ lib/themes_on_rails/railtie.rb
@@ -9,11 +9,13 @@ module ThemesOnRails
 
     initializer "themes_on_rails.assets_path" do |app|
       Dir.glob("#{Rails.root}/app/themes/*/assets/*").each do |dir|
+        puts dir.inspect
         app.config.assets.paths << dir
       end
     end
 
     initializer "themes_on_rails.precompile" do |app|
+      puts app.inspect
       app.config.assets.precompile += [ Proc.new { |path, fn| fn =~ /app\/themes/ && !%w(.js .css).include?(File.extname(path)) } ]
       app.config.assets.precompile += Dir["app/themes/*"].map { |path| "#{path.split('/').last}/all.js" }
       app.config.assets.precompile += Dir["app/themes/*"].map { |path| "#{path.split('/').last}/all.css" }
